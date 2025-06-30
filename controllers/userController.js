9539afb11c7a7da3565c6f5f60c5487002de093b
davidwole <davidwole20@gmail.com>
2025-06-30 22:21:40 +0100

    Fixed status code

--- controllers/userController.js
+++ controllers/userController.js
@@ -118,7 +118,7 @@ exports.login = async (req, res) => {
 exports.getAllUsers = async (req, res) => {
   try {
     const users = await User.find();
-    res.status(500).json(users);
+    res.status(200).json(users);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
